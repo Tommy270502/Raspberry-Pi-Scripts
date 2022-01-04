6ee8b06839b3c601c8f3c008000b64665a80d880
Thomas S. Perri <87670767+Tommy270502@users.noreply.github.com>
2022-01-04 11:23:39 +0100

    updated measure CPU temp&freq.py
    Function added to clear terminal after every measurement.

--- Raspberry Pi Scripts/measure CPU temp&freq.py
+++ Raspberry Pi Scripts/measure CPU temp&freq.py
@@ -4,11 +4,11 @@
 # Hardware : Raspberry Pi 4, Raspberry Pi Zero 2 W
 #
 # Description:
-# The script reads out the temperature and frequency of the CPU 
+# The script reads out the temperature and frequency of the CPU
 # and prints their values to the terminal.
 #
 # Date:       Author:           Version
-# 16.12.2021  Thomas S. Perri   V1.0
+# 04.01.2021  Thomas S. Perri   V1.1
 #*********************************************************************************
 
 import os
@@ -22,9 +22,17 @@ def measure_freq():
     cpu_freq = os.popen("vcgencmd measure_clock arm").readline()
     return cpu_freq.replace("freq=", "")
 
+def clear():
+    clear = os.popen("clear").readline()
+    return clear.replace("clear=", "")
+
 while True:
-    print("Status: connected")
+    print("-------------")
     print(temperature_of_raspberry_pi())
     print(measure_freq())
     print("-------------")
-    time.sleep(5)
+    time.sleep(1)
+    print(clear())
+    time.sleep(0.1)
+
+
